967a6209e696bdea186aa04a9ede4a4e71fcec58
lanemunds <lanemunds@gmail.com>
2022-09-07 15:08:04 -0600

    finalCommit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const {getHouses, deleteHouse, updateHouse, createHouse}= require('./controller')
+
+app.get('/api/houses', getHouses)
+app.delete('/api/houses/:id', deleteHouse)
+app.post('/api/houses', createHouse)
+app.put('/api/houses/:id', updateHouse)
+
+
+app.listen(4004, ()=>console.log('server running on 4004'))
